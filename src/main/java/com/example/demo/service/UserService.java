4e0f6e556218adbb82cd9a9e90b1daaefdca551e
Jonas-Silva01 <140558999+Jonas-Silva01@users.noreply.github.com>
2023-08-19 12:22:42 -0400

    findById method and error treatment

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entities.User;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> u = repository.findById(id);
+		return u.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
