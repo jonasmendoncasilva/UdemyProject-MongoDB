092fdf3f162fb26ae135562dd238f511bdb1780a
Jonas-Silva01 <140558999+Jonas-Silva01@users.noreply.github.com>
2023-08-19 11:22:54 -0400

    User repository, changes in user service and connect on MongoDB

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.User;
+import com.example.demo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+}
